cc3cefed7f6367dca55ca0b4b1f26cd2c2e8ddc1
Stathis <predatorlive@gmail.com>
2020-02-06 14:42:42 +0200

    added dist folder

---
+++ dist/index.js
@@ -0,0 +1,52 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Responsive", {
+  enumerable: true,
+  get: function get() {
+    return _Responsive.Responsive;
+  }
+});
+Object.defineProperty(exports, "getWindowDimension", {
+  enumerable: true,
+  get: function get() {
+    return _utilResponsive.getWindowDimension;
+  }
+});
+Object.defineProperty(exports, "getDeviceTypeInfo", {
+  enumerable: true,
+  get: function get() {
+    return _utilResponsive.getDeviceTypeInfo;
+  }
+});
+Object.defineProperty(exports, "isMobileDevice", {
+  enumerable: true,
+  get: function get() {
+    return _utilResponsive.isMobileDevice;
+  }
+});
+Object.defineProperty(exports, "isTabletDevice", {
+  enumerable: true,
+  get: function get() {
+    return _utilResponsive.isTabletDevice;
+  }
+});
+Object.defineProperty(exports, "isLaptopDevice", {
+  enumerable: true,
+  get: function get() {
+    return _utilResponsive.isLaptopDevice;
+  }
+});
+Object.defineProperty(exports, "isBiggerThanLaptop", {
+  enumerable: true,
+  get: function get() {
+    return _utilResponsive.isBiggerThanLaptop;
+  }
+});
+
+var _Responsive = require("./Responsive");
+
+var _utilResponsive = require("./utilResponsive");
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7QUFBQTs7QUFDQSIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IFJlc3BvbnNpdmUgfSBmcm9tIFwiLi9SZXNwb25zaXZlXCI7XG5pbXBvcnQge1xuICBnZXRXaW5kb3dEaW1lbnNpb24sXG4gIGdldERldmljZVR5cGVJbmZvLFxuICBpc01vYmlsZURldmljZSxcbiAgaXNUYWJsZXREZXZpY2UsXG4gIGlzTGFwdG9wRGV2aWNlLFxuICBpc0JpZ2dlclRoYW5MYXB0b3Bcbn0gZnJvbSBcIi4vdXRpbFJlc3BvbnNpdmVcIjtcblxuZXhwb3J0IHtcbiAgUmVzcG9uc2l2ZSxcbiAgZ2V0RGV2aWNlVHlwZUluZm8sXG4gIGdldFdpbmRvd0RpbWVuc2lvbixcbiAgaXNNb2JpbGVEZXZpY2UsXG4gIGlzVGFibGV0RGV2aWNlLFxuICBpc0xhcHRvcERldmljZSxcbiAgaXNCaWdnZXJUaGFuTGFwdG9wXG59O1xuIl19
